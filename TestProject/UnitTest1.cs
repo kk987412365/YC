ede06336b50fd2171ae9e3a83fb5fc8234c10dd0
YiMing Huang <kk987412365@gmail.com>
2023-03-12 18:44:54 +0800

    加入專案檔案。

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+using YC.Controllers;
+namespace TestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+          
+        }
+    }
+}
